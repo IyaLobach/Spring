f8cc82552b4332377779734213a5eedc85ecc495
IyaLobach <lobach.iya@gmail.com>
2023-10-05 02:10:05 +0300

    Add hibernate-app1

---
+++ Hibernate/hibernate-app1/src/main/java/org/example/App.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.example.models.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration configuration = new Configuration().addAnnotatedClass(Person.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        session.beginTransaction();
+
+        Person person = session.get(Person.class, 1);
+        System.out.println(person);
+
+        session.getTransaction().commit();
+        sessionFactory.close();
+    }
+}
